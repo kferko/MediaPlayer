f2e0d14e6e6f10edc48aa93e1c27597fed0cf50a
Kevin Ferko <kevinferko@gmail.com>
2016-01-24 10:36:37 -0500

    stage 1

--- js/playlistdiv.js
+++ js/playlistdiv.js
@@ -8,6 +8,7 @@ $("#radams1").click(function(){
 	for (var i = 0; i < radams1.length; i++){
 		$("#songlist").find("ul").append("<li class='item'>" + radams1[i].name + "</li>");
 	}
+  $("#albumart1").fadeIn("slow");
 });
 $("#radams2").click(function(){
 	// call the addSong function
@@ -17,6 +18,8 @@ $("#radams2").click(function(){
 	for (var i = 0; i < radams2.length; i++){
 		$("#songlist").find("ul").append("<li class='item'>" + radams2[i].name + "</li>");
 	}
+  $("#albumart1").fadeToggle();
+  $("#albumart2").fadeIn("slow");
 });
 
 // click a song in the list and make it the current song
@@ -28,6 +31,58 @@ $("ul").on('click', 'li.item', function(){
 	juke.setSong(juke.playlist[juke.currentIndex]);
 	juke.play();
 });
+
+
+// click expands a div's height to show full content. 
+// going to have to use a nested set
+
+$('#pldiv1').on({
+    mouseenter: function() {
+      $(this).css({border: '0 solid rgba(255,255,255, 0.4)'}).animate({borderWidth: 1});
+      $(this).animate({height:$('#radams1').height(),}), 500;},
+    mouseleave: function() {
+      $(this).animate({height:30}), 500;
+      $(this).animate({borderWidth: 0});
+    }
+});
+$('#pldiv2').on({
+    mouseenter: function() {
+      $(this).css({border: '0 solid rgba(255,255,255, 0.4)'}).animate({borderWidth: 1});
+      $(this).animate({height:$('#radams2').height(),}), 500;},
+    mouseleave: function() {
+      $(this).animate({height:30}), 500;
+      $(this).animate({borderWidth: 0});
+    }
+});
+
+// full screen background - youtube video
+$('#video').YTPlayer({
+    fitToBackground: true,
+    videoId: 'SL6d4rQJsQY',
+    playerVars: {
+      modestbranding: 0,
+      autoplay: 1,
+      controls: 0,
+      showinfo: 0,
+      branding: 0,
+      rel: 0,
+      autohide: 0,
+      loopPlaylists: false,
+      start: 10
+    }
+});
+
+// image over top of bkg video fade up and down over time
+var vol1 = $('#vidoverlay1');
+function fadeIt() {
+       vol1.animate({opacity:'0.4'}, 5000);
+       vol1.animate({opacity:'0.1'}, 2000);
+       vol1.animate({opacity:'0.6'}, 8000, fadeIt);
+    }
+fadeIt();
+
+
+
 
 
 });
--- js/song.js
+++ js/song.js
@@ -7,18 +7,20 @@ function Song(name,url){
 // create a playlist
 var adams1 = new Song("She Wants To Play Hearts", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d1t07.mp3");
 var adams2 = new Song("Damn Sam (I Love a Woman That Rains)", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d2t02.mp3");
+var adams3 = new Song("Firecracker", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d2t06.mp3");
+var adams4 = new Song("Harder Now That It's Over", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d1t10.mp3");
 
 // name the playlist to be added and its elements - name it according to name of div
-var radams1 =[adams1,adams2];
+var radams1 =[adams1,adams2,adams3,adams4];
 	for (var i = 0; i < radams1.length; i++){
 		$("#radams1").find("ul").append("<li>" + radams1[i].name + "</li>");
 	}
 
 // create another playlist
-var adams3 = new Song("Firecracker", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d2t06.mp3");
-var adams4 = new Song("Harder Now That It's Over", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d1t10.mp3");
+var adams5 = new Song("Hallelujah", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d1t03.mp3");
+var adams6 = new Song("New York, New York", "https://archive.org/download/ryanadams2006-10-17.sbd.flac16/ryanadams2006-10-17.sbd.d1t06.mp3");
 // name the playlist to be added and its elements
-var radams2 =[adams3,adams4];
+var radams2 =[adams5,adams6];
 	for (var i = 0; i < radams2.length; i++){
 		$("#radams2").find("ul").append("<li>" + radams2[i].name + "</li>");
 	}
